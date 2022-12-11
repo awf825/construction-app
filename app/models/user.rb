da74f9e9a020073302425ada2cff2986830d24ca
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2022-12-11 15:43:22 -0500

    added custom values to jwt payload

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def jwt_payload
+    super.merge(
+      'name' => self.name,
+      'email' => self.email
+    )
+  end
+
 end
